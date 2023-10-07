ce089f9e848109f2c2fa73adc9daf67191e2c4c3
Fex-zi <fexzi87@gmail.com>
2023-10-07 10:13:24 -0600

    Auth

--- app.js
+++ app.js
@@ -53,17 +53,54 @@ const authenticate = (req, res, next) => {
 
 // POST: Create a new item
 app.post('/api/items', authenticate, async (req, res) => {
- 
+  try {
+    const { username, displayName } = req.body; 
+    if (!username || !displayName) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
+
+    const user = new User({ username, displayName });
+    await user.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // PUT: Update an item
 app.put('/api/items/:id', authenticate, async (req, res) => {
-  
+  try {
+    const { username, displayName } = req.body; 
+    if (!username && !displayName) {
+      return res.status(400).json({ error: 'Bad Request' });
+    }
+
+    const updatedUser = await User.findByIdAndUpdate(req.params.id, { username, displayName }, { new: true });
+    if (!updatedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(200).json({ message: 'User updated successfully', updatedUser });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // DELETE: Delete an item
 app.delete('/api/items/:id', authenticate, async (req, res) => {
-  
+  try {
+    const deletedUser = await User.findByIdAndDelete(req.params.id);
+    if (!deletedUser) {
+      return res.status(404).json({ error: 'User not found' });
+    }
+
+    res.status(204).json({ message: 'User deleted successfully' });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 // Serve Swagger UI
